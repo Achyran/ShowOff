02b32827083f0d3576e4b1d8686df2977f33f24a
Achran <tobias.breitkopf@googlemail.com>
2021-05-17 14:19:20 +0200

    Added the first posession mecanic

--- Assets/Scripts/Tobi/FishPossetion/MovementMaster.cs
+++ Assets/Scripts/Tobi/FishPossetion/MovementMaster.cs
@@ -8,11 +8,14 @@ public class MovementMaster : MonoBehaviour
     public static MovementMaster current;
     [SerializeField]
     private IPosessable currentContorl;
+    [SerializeField]
+    private float _possesingTime;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        if(current != null)
+        if(current == null)
         {
             current = this;
         }
@@ -23,22 +26,37 @@ public class MovementMaster : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update is called once per physicsUpdate
+    void FixedUpdate()
     {
+
         if (currentContorl != null)
         {
             currentContorl.Move();
         }
-    }
 
-    public event Action event_Posess;
-
-    public void Posess()
-    {
-        if(event_Posess != null)
+        if (_possesingTime > 0)
         {
-
+            _possesingTime -= Time.deltaTime;
+        }else if(currentContorl != null)
+        {
+            RemovePosession();
         }
+
+
+    }
+    public void Posess(IPosessable posessable)
+    {
+        if (currentContorl == null && posessable != null)
+        {
+            currentContorl = posessable;
+            _possesingTime = posessable.time;
+            posessable.isPosessed = true;
+        }
+    }
+    public void RemovePosession()
+    {
+        currentContorl.isPosessed = false;
+        currentContorl = null;
     }
 }
