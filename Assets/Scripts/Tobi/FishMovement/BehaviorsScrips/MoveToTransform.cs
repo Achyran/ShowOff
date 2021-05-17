3acb539c5287b97de599f0b13dd2e952057603b4
Achran <tobias.breitkopf@googlemail.com>
2021-05-17 16:44:00 +0200

    Added Follow point array to behavoir

---
+++ Assets/Scripts/Tobi/FishMovement/BehaviorsScrips/MoveToTransform.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/MoveToPoitList")]
+public class MoveToTransform : FlockBehavior
+{
+    [SerializeField]
+    private Vector3[] point;
+    [SerializeField]
+    [Range(0f, 10f)]
+    private float strenght;
+    [SerializeField]
+    private float radius;
+    private int index;
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> ctx, Flock flock)
+    {
+        Vector3 offset = point[index] - agent.transform.position;
+
+        if (offset.sqrMagnitude < radius * radius)
+        {
+            index++;
+            if(index >= point.Length)
+            {
+                index = 0;
+            }
+
+            return Vector3.zero;
+        }
+        return offset.normalized * strenght;
+
+    }
+}
