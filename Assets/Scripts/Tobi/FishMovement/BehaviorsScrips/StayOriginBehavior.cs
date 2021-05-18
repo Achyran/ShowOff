c9ecbab3febb88e09ffdff81b51d66f06ffcfa75
Achran <tobias.breitkopf@googlemail.com>
2021-05-18 10:21:10 +0200

    Added Raycast Behavior to Flock

--- Assets/Scripts/Tobi/FishMovement/BehaviorsScrips/StayOriginBehavior.cs
+++ Assets/Scripts/Tobi/FishMovement/BehaviorsScrips/StayOriginBehavior.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class StayOriginBehavior : FlockBehavior
 {
     [SerializeField]
-    private Vector3 origin;
+    public Vector3 origin;
     [SerializeField]
     private float raidus;
 
