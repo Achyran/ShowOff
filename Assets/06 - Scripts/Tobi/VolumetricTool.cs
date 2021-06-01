82a92cd6bbff5f14c5c69bbb696f0175203ddfb5
Achyran <tobias.breitkopf@googlemail.com>
2021-06-01 14:50:20 +0200

    Added Prefab As a Mash
    The Tool now previes the Prefab mash as a gismo

--- Assets/06 - Scripts/Tobi/VolumetricTool.cs
+++ Assets/06 - Scripts/Tobi/VolumetricTool.cs
@@ -11,11 +11,17 @@ public class VolumetricTool : MonoBehaviour
     [SerializeField]
     private bool draw;
     [SerializeField]
+    private GameObject prefab;
     private Mesh mesh;
     [SerializeField]
     private bool previewMesh;
+    [SerializeField]
+    private Vector3 meshRotation;
+    [SerializeField]
+    private Vector3 meshScale;
     private PlaneTransform oldArea;
     private Plane _plane;
+    private GameObject _oldPrefab;
 
     [System.Serializable]
     private struct PlaneTransform
@@ -28,18 +34,43 @@ public class VolumetricTool : MonoBehaviour
     {
         if (draw)
         {
+
             ReCalculatePlane();
-            Gizmos.color = new Color(0, 1, 0,0.5f);
+            UpdatePrefab();
+            Gizmos.color = new Color(0, 1, 0,0.3f);
             Gizmos.DrawCube(area.position,new Vector3(area.scale.x *10,0.1f, area.scale.y *10));
             Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
             Vector3 hitpos = hitPlane(ray);
             if(hitpos != Vector3.zero)
             {
-                Gizmos.DrawCube(hitpos, Vector3.one);
+                if (mesh == null)
+                    Gizmos.DrawCube(hitpos, Vector3.one);
+                else
+                {
+                    Gizmos.DrawMesh(mesh,hitpos,Quaternion.Euler(meshRotation), meshScale);
+                }
             }
             
 
         }
+    }
+
+
+    private void UpdatePrefab()
+    {
+        if(prefab != _oldPrefab)
+        {
+            _oldPrefab = prefab;
+            meshScale = prefab.transform.localScale;
+            meshRotation =  prefab.transform.localRotation.eulerAngles;
+            MeshFilter meshFilter = prefab.GetComponent<MeshFilter>();
+            mesh = meshFilter.sharedMesh;
+            if (mesh != null)
+            {
+                mesh.RecalculateNormals();
+            }
+        }
+        
     }
 
     private void ReCalculatePlane()
@@ -56,6 +87,8 @@ public class VolumetricTool : MonoBehaviour
         float dist;
         if (_plane.Raycast(pRay,out dist))
         {
+            
+
             Vector3 output = pRay.origin + pRay.direction * dist;
             return output;
         }
