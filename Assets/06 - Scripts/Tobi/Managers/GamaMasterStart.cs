ea539bcbb3713c061c22724c04d579ffe956d699
Achyran <tobias.breitkopf@googlemail.com>
2021-06-16 17:11:53 +0200

    Fixed start function in GameMaster

--- Assets/06 - Scripts/Tobi/Managers/GamaMasterStart.cs
+++ Assets/06 - Scripts/Tobi/Managers/GamaMasterStart.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class GamaMasterStart : MonoBehaviour
 {
+    bool triggerd;
     void Awake()
     {
-        if (GameMaster.current != null) GameMaster.current.ScenneStart();
+        if (GameMaster.current != null && !triggerd)
+        {
+            triggerd = true;
+            GameMaster.current.ScenneStart();
+        }
+    }
+    private void Start()
+    {
+        if (GameMaster.current != null && !triggerd)
+        {
+            triggerd = true;
+            GameMaster.current.ScenneStart();
+        }
+
+        if (!triggerd) Debug.LogError("Could not inform GameMaster");
     }
 
 }
