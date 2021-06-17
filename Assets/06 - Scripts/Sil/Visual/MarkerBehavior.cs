49154f9b05c9dcc6c92c64771160926d22c610f8
Achyran <tobias.breitkopf@googlemail.com>
2021-06-17 16:37:35 +0200

    REImporting changes

--- Assets/06 - Scripts/Sil/Visual/MarkerBehavior.cs
+++ Assets/06 - Scripts/Sil/Visual/MarkerBehavior.cs
@@ -92,7 +92,7 @@ public class MarkerBehavior : MonoBehaviour
     {
 
 		float distance = Vector3.Distance(transform.position, player.transform.position);
-		Debug.Log(distance);
+		//Debug.Log(distance);
 
 		if(distance < fadeDist)
         {
