2f3f9981ad61b2b7195b19aec6f6e0d54af5e3e7
Achyran <tobias.breitkopf@googlemail.com>
2021-06-24 17:25:47 +0200

    Added Discoverabl box trigger and fix notebook button in lv2

---
+++ Assets/06 - Scripts/Tobi/Discover/DiscoverableTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class DiscoverableTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private DiscoverableMaster.Species species;
+    private BoxCollider col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+        col.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Calling Discover");
+        if(other.gameObject == GameMaster.current.player.gameObject)
+        {
+            DiscoverableMaster.current.AddToDisvcovered(species);
+        }
+    }
+}
